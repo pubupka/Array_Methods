971098ce2955903ccd7b99b3d8ae21ee5d37057a
Kakapuk <Kakapuk@LAPTOP-GNU5QET2>
2023-03-15 19:31:35 +0600

    Добавьте файлы проекта.

---
+++ Methods/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace Methods
+{
+    public class Collections_Methods
+    {
+        public class Array_Methods : Collections_Methods 
+        {
+            public static int[] Sort(int[] arr) // Sort
+            {
+                Array.Sort(arr);
+                return arr;
+            }
+
+            public static int[] Reverse(int[] arr) // Reverse
+            {
+                Array.Reverse(arr);
+                return arr;
+            }
+
+            public static void BinarySearch(int[] arr) // BinarySearch
+            {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                int[] copy = new int[arr.Length];
+                arr.CopyTo(copy, 0);
+                Array.Sort(copy);
+
+                bool flag = true;
+
+                for (int i=0; i<copy.Length; i++)
+                {
+                    if (copy[i] != arr[i])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                {
+                    Console.WriteLine("Какое число найти?");
+                    int number = int.Parse(Console.ReadLine());
+                    int index = Array.BinarySearch(arr, number);
+                    Console.WriteLine("Искомый индекс = " + index);
+                }
+                else
+                    Console.WriteLine("Массив не отсортирован");
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+
+            public static int[] Clear(int[] arr)
+            {
+                Array.Clear(arr);
+                return arr;
+            }
+
+            public static int[] SetValue(int[] arr)
+            {
+                Console.Clear();
+                
+                Console.WriteLine("По какому индексу изменить значение?");
+                int index = int.Parse(Console.ReadLine());
+                Console.WriteLine("Какое значение присвоить?");
+                int value = int.Parse(Console.ReadLine());
+
+                arr.SetValue(value, index);
+
+                Console.Clear();
+
+                return arr;
+            }
+
+            public static void IndexOf(int[] arr)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Индекс какого элемента показать?");
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine("Индекс первого вхождения: {0}", Array.IndexOf(arr, value));
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+            public static void LastIndexOf(int[] arr)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Индекс какого элемента показать?");
+                int value = int.Parse(Console.ReadLine());
+                Console.WriteLine("Индекс последнего вхождения: {0}", Array.LastIndexOf(arr, value));
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+            public static void GetValue(int[] arr)
+            {
+                Console.Clear();
+
+                Console.WriteLine("По какому индексу получить значение?");
+                int index = int.Parse(Console.ReadLine());
+                Console.WriteLine("Значение по данному индексу: {0}", arr.GetValue(index));
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+            public static void Length(int[] arr)
+            {
+                Console.Clear();
+                Console.WriteLine("Длина массива: {0} элементов", arr.Length);
+
+                Console.WriteLine("Нажмите любую клавишу");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+            static void Main()
+            {
+
+            }
+        }
+        
+    }
+}
---
+++ Методы Array/Program.cs
@@ -0,0 +1,97 @@
+﻿using static Methods.Collections_Methods.Array_Methods;
+
+namespace Методы_Array
+{
+    internal class Program
+    {
+        static int Move(int i, int n, ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                i = (n + i - 1) % n;
+            }
+
+            if (key.Key == ConsoleKey.DownArrow)
+            {                
+                i = (i + 1) % n;
+            }
+
+            return i;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+            int i = 0, n = 9;
+
+            while (true)
+            {
+                
+                Console.SetCursorPosition(0, 11);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Массив: [" + String.Join(", ", array) + "]");
+
+                Console.SetCursorPosition(0, 0);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Отсортировать по возрастанию"); //Sort
+                Console.WriteLine("Перевернуть"); // Reverse
+                Console.WriteLine("Бинарный поиск"); // BinarySearch
+                Console.WriteLine("Очистить"); // Clear
+                Console.WriteLine("Установить значение"); // SetValue
+                Console.WriteLine("Индекс первого вхождения элемента"); // IndexOf
+                Console.WriteLine("Индекс последнего вхождения элемента"); // LastIndexOf
+                Console.WriteLine("Получить значение по индексу"); // GetValue
+                Console.WriteLine("Длина массива"); // Это вообще свойство, но я не знаю, что ещё добавить
+                Console.SetCursorPosition(40, i);
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                if (i == 0 && key.Key == ConsoleKey.Enter)
+                {
+                        array = Sort(array);
+                }
+
+                if (i == 1 && key.Key == ConsoleKey.Enter)
+                {
+                    array = Reverse(array);
+                }
+
+                if (i == 2 && key.Key == ConsoleKey.Enter)
+                {
+                    BinarySearch(array);
+                }
+
+                if (i == 3 && key.Key == ConsoleKey.Enter)
+                {
+                    array = Clear(array);
+                }
+
+                if (i == 4 && key.Key == ConsoleKey.Enter)
+                {
+                    array = SetValue(array);
+                }
+
+                if (i == 5 && key.Key == ConsoleKey.Enter)
+                {
+                    IndexOf(array);
+                }
+
+                if (i == 6 && key.Key == ConsoleKey.Enter)
+                {
+                    LastIndexOf(array);
+                }
+
+                if (i == 7 && key.Key == ConsoleKey.Enter)
+                {
+                    GetValue(array);
+                }
+
+                if (i == 8 && key.Key == ConsoleKey.Enter)
+                {
+                    Length(array);
+                }
+
+                i = Move(i, n, key);
+            }
+        }
+    }
+}
